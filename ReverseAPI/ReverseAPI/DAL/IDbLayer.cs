ce2188d612b01540130c5f567c6421da16ed8f03
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-03-14 00:08:24 +0100

    add db communication

---
+++ ReverseAPI/ReverseAPI/DAL/IDbLayer.cs
@@ -0,0 +1,23 @@
+﻿using ReverseAPI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReverseAPI.DAL
+{
+    interface IDbLayer
+    {
+        // Client
+        public Task<IEnumerable<Client>> GetClients();
+        public Task<Client> GetClient(int? id);
+        public Task<Client> AddClient(Client newCLient);
+        public Task<Client> UpdateClient(Client clientToEdit);
+        public Task<Client> DeleteClient(int? id);
+
+        // Supplier
+        public Task<IEnumerable<Supplier>> GetSuppliers();
+        public Task<Supplier> GetSupplier(int? id);
+        public Task<Supplier> AddSupplier(Supplier newSupplier);
+        public Task<Supplier> UpdateSupplier(Supplier supplierToEdit);
+        public Task<Supplier> DeleteSupplier(int? id);
+    }
+}
---
+++ ReverseAPI/ReverseAPI/DAL/SqlDbLayer.cs
@@ -0,0 +1,65 @@
+﻿using ReverseAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReverseAPI.DAL
+{
+    public class SqlDbLayer : IDbLayer
+    {
+        public readonly MainContext _context;
+        
+        // Clients
+        public async Task<IEnumerable<Client>> GetClients()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Client> GetClient(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Client> AddClient(Client newClient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Client> UpdateClient(Client clientToEdit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Client> DeleteClient(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Suppliers
+        public async Task<IEnumerable<Supplier>> GetSuppliers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Supplier> GetSupplier(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Supplier> AddSupplier(Supplier newSupplier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Supplier> UpdateSupplier(Supplier supplierToEdit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Supplier> DeleteSupplier(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}

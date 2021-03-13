bdb7239f57a525b8dea460ecdf5fa62c9fcd631f
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-03-13 23:22:44 +0100

    Added initial migration

---
+++ ReverseAPI/ReverseAPI/DAL/MainContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReverseAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReverseAPI.DAL
+{
+    public class MainContext : DbContext
+    {
+        public MainContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Client> Clients { get; set; }
+        public virtual DbSet<Supplier> Suppliers { get; set; }
+    }
+}
--- ReverseAPI/ReverseAPI/Startup.cs
+++ ReverseAPI/ReverseAPI/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using ReverseAPI.DAL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,10 +30,9 @@ namespace ReverseAPI
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ReverseAPI", Version = "v1" });
-            });
+
+            services.AddDbContext<MainContext>(option => 
+                option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

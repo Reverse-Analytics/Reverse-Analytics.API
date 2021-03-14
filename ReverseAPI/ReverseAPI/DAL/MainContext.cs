b8e5aca00ad373893d3bb2dc3f374b09d073750c
Miraziz Khidoyatov <49793609+Mirazyzz@users.noreply.github.com>
2021-03-14 22:08:27 +0100

    add supply migration

--- ReverseAPI/ReverseAPI/DAL/MainContext.cs
+++ ReverseAPI/ReverseAPI/DAL/MainContext.cs
@@ -13,5 +13,6 @@ namespace ReverseAPI.DAL
         public virtual DbSet<Supplier> Suppliers { get; set; }
         public virtual DbSet<Product> Products { get; set; }
         public virtual DbSet<Purchase> Purchases { get; set; }
+        public virtual DbSet<Supply> Supplies { get; set; }
     }
 }
--- ReverseAPI/ReverseAPI/Migrations/20210314210800_Added supply model.Designer.cs
+++ ReverseAPI/ReverseAPI/Migrations/20210314210800_Added supply model.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReverseAPI.DAL;
 
 namespace ReverseAPI.Migrations
 {
     [DbContext(typeof(MainContext))]
-    partial class MainContextModelSnapshot : ModelSnapshot
+    [Migration("20210314210800_Added supply model")]
+    partial class Addedsupplymodel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

9a05635bf92fb4e200bbe8c5d9a32c5a5fc1fbb2
Mirazyzz <khidoyatov1997@gmail.com>
2022-05-24 15:23:15 +0200

    add common entities & events

---
+++ Reverse-Analytics/ReverseAnalytics.Domain/Common/BaseEntity.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReverseAnalytics.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+
+        private readonly List<BaseEvent> _domainEvents = new List<BaseEvent>();
+
+        [NotMapped]
+        public IReadOnlyCollection<BaseEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+        public void AddDomainEvent(BaseEvent domainEvent)
+        {
+            if (domainEvent == null)
+                return;
+
+            if (_domainEvents.Contains(domainEvent))
+                return;
+
+            _domainEvents.Add(domainEvent);
+        }
+
+        public void RemoveDomainEvent(BaseEvent domainEvent)
+        {
+            if (domainEvent == null)
+                return;
+
+            if (!_domainEvents.Contains(domainEvent))
+                return;
+
+            _domainEvents.Remove(domainEvent);
+        }
+
+        public void ClearDomainEvents()
+        {
+            _domainEvents.Clear();
+        }
+    }
+}
---
+++ Reverse-Analytics/ReverseAnalytics.Domain/Common/BaseEvent.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace ReverseAnalytics.Domain.Common
+{
+    public abstract class BaseEvent : INotification
+    {
+    }
+}

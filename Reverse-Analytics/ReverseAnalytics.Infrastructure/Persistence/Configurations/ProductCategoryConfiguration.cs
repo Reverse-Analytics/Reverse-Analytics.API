b85c57d9308d2f66dc72dbb77cedc12ebf3f12e5
Mirazyzz <khidoyatov1997@gmail.com>
2022-05-24 16:24:42 +0200

    add product & product category configurations

---
+++ Reverse-Analytics/ReverseAnalytics.Infrastructure/Persistence/Configurations/ProductCategoryConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReverseAnalytics.Domain.Entities;
+
+namespace ReverseAnalytics.Infrastructure.Persistence.Configurations
+{
+    public class ProductCategoryConfiguration : IEntityTypeConfiguration<ProductCategory>
+    {
+        public void Configure(EntityTypeBuilder<ProductCategory> builder)
+        {
+            builder.HasMany(pc => pc.Products)
+                .WithOne(p => p.Category);
+
+            builder.Property(pc => pc.CategoryName)
+                .HasMaxLength(200)
+                .IsRequired();
+        }
+    }
+}
---
+++ Reverse-Analytics/ReverseAnalytics.Infrastructure/Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ReverseAnalytics.Domain.Entities;
+
+namespace ReverseAnalytics.Infrastructure.Persistence.Configurations
+{
+    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(p => p.Category)
+                .WithMany(pc => pc.Products)
+                .HasForeignKey(p => p.CategoryId)
+                .HasConstraintName("Category_Id");
+
+            builder.Property(p => p.ProductName)
+                .HasMaxLength(250)
+                .IsRequired();
+            builder.Property(p => p.PurchasePrice)
+                .HasColumnType("money");
+            builder.Property(p => p.SalePrice)
+                .HasColumnType("money");
+        }
+    }
+}

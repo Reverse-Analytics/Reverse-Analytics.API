241620e345fa2b1441563b1ca0520fceca17ef56
Mirazyzz <khidoyatov1997@gmail.com>
2022-05-26 20:27:32 +0200

    add base repository implementation

--- Reverse-Analytics/ReverseAnalytics.Domain/Interfaces/Repositories/IBaseRepository.cs
+++ Reverse-Analytics/ReverseAnalytics.Domain/Interfaces/Repositories/IBaseRepository.cs
@@ -3,11 +3,11 @@
     public interface IBaseRepository<T> where T : class
     {
         IEnumerable<T> FindAll();
-        Task<T> FindById(int id);
-        T Create(T entity);
-        Task CreateRange(IEnumerable<T> entities);
+        Task<T?> FindById(int id);
+        void Create(T entity);
         void Update(T entity);
         void Delete(T entity);
         Task<bool> SaveChangesAsync();
+        Task<bool> EntityExists(int id);
     }
 }
---
+++ Reverse-Analytics/ReverseAnalytics.Repositories/BaseRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReverseAnalytics.Domain.Interfaces.Repositories;
+using ReverseAnalytics.Infrastructure.Persistence;
+
+namespace ReverseAnalytics.Repositories
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _context;
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<T> FindAll()
+        {
+            return _context.Set<T>().AsNoTracking();
+        }
+
+        public async Task<T?> FindById(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public void Create(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _context.SaveChangesAsync() >= 0);
+        }
+
+        public async Task<bool> EntityExists(int id)
+        {
+            return await _context.Set<T>().FindAsync(id) != null;
+        }
+    }
+}

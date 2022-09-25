34249b8ee4c57805220388d726e8a4f96c37e91f
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2022-09-25 14:55:10 +0500

    Add customer debt DTOs & mapping profile

---
+++ Reverse-Analytics/ReverseAnalytics.Domain/DTOs/CustomerDebt/CustomerDebtForCreate.cs
@@ -0,0 +1,11 @@
+﻿namespace ReverseAnalytics.Domain.DTOs.CustomerDebt
+{
+    public class CustomerDebtForCreate
+    {
+        public decimal Amount { get; set; }
+        public DateTime DebtDate { get; set; }
+        public DateTime DueDate { get; set; }
+        
+        public int CustomerId { get; set; }
+    }
+}
---
+++ Reverse-Analytics/ReverseAnalytics.Domain/DTOs/CustomerDebt/CustomerDebtForUpdate.cs
@@ -0,0 +1,10 @@
+﻿namespace ReverseAnalytics.Domain.DTOs.CustomerDebt
+{
+    public class CustomerDebtForUpdate
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime DebtDate { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}

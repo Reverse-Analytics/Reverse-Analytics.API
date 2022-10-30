91763f70e0b0c311818a4d552fea6e141f2c881c
Mirazyzz <khidoyatov1997@gmail.com>
2022-10-30 15:17:23 +0100

    Add Supplier Debt Repository

---
+++ Reverse-Analytics/ReverseAnalytics.Repositories/SupplierDebtRepository.cs
@@ -0,0 +1,14 @@
+﻿using ReverseAnalytics.Domain.Entities;
+using ReverseAnalytics.Domain.Interfaces.Repositories;
+using ReverseAnalytics.Infrastructure.Persistence;
+
+namespace ReverseAnalytics.Repositories
+{
+    public class SupplierDebtRepository : RepositoryBase<SupplierDebt>, ISupplierDebtRepository
+    {
+        public SupplierDebtRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+    }
+}

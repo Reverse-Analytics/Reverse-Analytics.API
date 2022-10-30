55640f6371a9d7797767699cbfa12ac12fe6fe1c
Mirazyzz <khidoyatov1997@gmail.com>
2022-10-30 15:15:38 +0100

    Add Supplier Debt Dtos

---
+++ Reverse-Analytics/ReverseAnalytics.Domain/DTOs/SupplierDebt/SupplierDebtForUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ReverseAnalytics.Domain.DTOs.SupplierDebt
+{
+    public class SupplierDebtForUpdateDto
+    {
+        public decimal Amount { get; set; }
+        public DateTime DueDate { get; set; }
+        public int SupplierId { get; set; }
+    }
+}

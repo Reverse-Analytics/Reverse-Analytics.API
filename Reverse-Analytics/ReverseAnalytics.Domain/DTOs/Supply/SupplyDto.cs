3d8525badb4f8c6de4971f77424d3e1f50e693da
Mirazyzz <khidoyatov1997@gmail.com>
2022-10-31 16:56:30 +0100

    Add Supplier Dtos

---
+++ Reverse-Analytics/ReverseAnalytics.Domain/DTOs/Supply/SupplyDto.cs
@@ -0,0 +1,17 @@
+﻿using ReverseAnalytics.Domain.DTOs.Supplier;
+
+namespace ReverseAnalytics.Domain.DTOs.Supply
+{
+    public class SupplyDto
+    {
+        public int Id { get; set; }
+        public DateTime? PurchaseDate { get; set; }
+        public decimal TotalDue { get; set; }
+        public decimal PaymentAmount { get; set; }
+        public decimal? DebtAmount { get; set; }
+        public string? ReceivedBy { get; set; }
+
+        public int SupplierId { get; set; }
+        public SupplierDto Supplier { get; set; }
+    }
+}
---
+++ Reverse-Analytics/ReverseAnalytics.Domain/DTOs/Supply/SupplyForCreate.cs
@@ -0,0 +1,13 @@
+﻿namespace ReverseAnalytics.Domain.DTOs.Supply
+{
+    public class SupplyForCreate
+    {
+        public DateTime? PurchaseDate { get; set; }
+        public decimal TotalDue { get; set; }
+        public decimal PaymentAmount { get; set; }
+        public decimal? DebtAmount { get; set; }
+        public string? ReceivedBy { get; set; }
+
+        public int SupplierId { get; set; }
+    }
+}

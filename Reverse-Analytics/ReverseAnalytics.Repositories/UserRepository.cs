01d45fc2b5263899633a2452afb242e9ed9dd947
Mirazyzz <khidoyatov1997@gmail.com>
2022-11-06 14:54:12 +0100

    Add User repository

---
+++ Reverse-Analytics/ReverseAnalytics.Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using ReverseAnalytics.Domain.Entities;
+using ReverseAnalytics.Domain.Interfaces.Repositories;
+using ReverseAnalytics.Infrastructure.Persistence;
+
+namespace ReverseAnalytics.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+        public UserRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+    }
+}

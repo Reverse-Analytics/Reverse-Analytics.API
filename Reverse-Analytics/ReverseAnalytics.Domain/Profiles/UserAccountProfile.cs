cad6d9159736806cfe155da8a3891b81c5be7664
Mirazyzz <khidoyatov1997@gmail.com>
2022-11-21 07:13:30 +0100

    Add User Account mapper

---
+++ Reverse-Analytics/ReverseAnalytics.Domain/Profiles/UserAccountProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using ReverseAnalytics.Domain.DTOs.UserAccount;
+
+namespace ReverseAnalytics.Domain.Profiles
+{
+    internal class UserAccountProfile : Profile
+    {
+        public UserAccountProfile()
+        {
+            CreateMap<IdentityUser, UserAccountDto>();
+            CreateMap<UserAccountDto, IdentityUser>();
+            CreateMap<UserAccountForCreateDto, IdentityUser>();
+            CreateMap<UserAccountForUpdateDto, IdentityUser>();
+        }
+    }
+}

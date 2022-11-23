7f3959fe56e3c353aa7f293d2ceaa528df485f23
Mirazyzz <khidoyatov1997@gmail.com>
2022-11-23 08:44:47 +0100

    Revert "Add authentication models"
    This reverts commit 8428cfae2f26fc014fb7830b9caf54602f037598.

--- Reverse-Analytics/ReverseAnalytics.Domain/DTOs/Authentication/AuthenticationRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ReverseAnalytics.Domain.DTOs.Authentication
-{
-    public class AuthenticationRequest
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-}

addc895cc27bd46c4788089e6dda94dc32ff3982
Mirazyzz <khidoyatov1997@gmail.com>
2022-11-23 08:47:43 +0100

    Register Authentication defaults

--- Reverse-Analytics/Reverse-Analytics.Api/Extensions/ConfigureServices.cs
+++ Reverse-Analytics/Reverse-Analytics.Api/Extensions/ConfigureServices.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using ReverseAnalytics.Domain.Interfaces.Repositories;
+using ReverseAnalytics.Infrastructure.Configurations;
 using ReverseAnalytics.Infrastructure.Persistence;
 using ReverseAnalytics.Infrastructure.Persistence.Interceptors;
 using ReverseAnalytics.Repositories;
+using System.Text;
 
 namespace Reverse_Analytics.Api.Extensions
 {
@@ -41,6 +45,31 @@ namespace Reverse_Analytics.Api.Extensions
                 options.UseSqlServer(configuration.GetConnectionString("DefaultIdentityConnection"), builder =>
                 builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 #endif
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var tokenOptions = configuration.GetSection("TokenOptions").Get<CustomTokenOptions>();
+
+            services.AddAuthentication(auth =>
+            {
+                auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = tokenOptions.Issuer,
+                    ValidateAudience = true,
+                    ValidAudience = tokenOptions.Audience,
+                    RequireExpirationTime = false,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenOptions.SecurityKey))
+                };
+            });
 
             return services;
         }

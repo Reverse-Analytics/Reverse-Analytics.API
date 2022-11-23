ad0a56be4c20119cd3161e251689c980e477f235
Mirazyzz <khidoyatov1997@gmail.com>
2022-11-23 08:47:25 +0100

    Add authentication service

--- Reverse-Analytics/Reverse-Analytics.Api/Extensions/RegisterDependencyInjections.cs
+++ Reverse-Analytics/Reverse-Analytics.Api/Extensions/RegisterDependencyInjections.cs
@@ -20,6 +20,7 @@ namespace Reverse_Analytics.Api.Extensions
             services.AddScoped<ISupplyDetailService, SupplyDetailService>();
             services.AddScoped<IAccountService, AccountService>();
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }

f4a0982df0e396726b192f7f54f3ef96635b6905
Mirazyzz <khidoyatov1997@gmail.com>
2022-11-24 15:10:10 +0100

    Add Phone repository implementation

---
+++ Reverse-Analytics/ReverseAnalytics.Domain/Interfaces/Repositories/IPhoneRepository.cs
@@ -0,0 +1,9 @@
+﻿using ReverseAnalytics.Domain.Entities;
+
+namespace ReverseAnalytics.Domain.Interfaces.Repositories
+{
+    public interface IPhoneRepository : IRepositoryBase<Phone>
+    {
+        public Task<IEnumerable<Phone>> FindAllByPersonId(int personId);
+    }
+}
---
+++ Reverse-Analytics/ReverseAnalytics.Repositories/PhoneRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReverseAnalytics.Domain.Entities;
+using ReverseAnalytics.Domain.Interfaces.Repositories;
+using ReverseAnalytics.Infrastructure.Persistence;
+
+namespace ReverseAnalytics.Repositories
+{
+    public class PhoneRepository : RepositoryBase<Phone>, IPhoneRepository
+    {
+        public PhoneRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Phone>> FindAllByPersonId(int personId)
+        {
+            var phones = await _context.Phones
+                .Where(p => p.PersonId == personId)
+                .ToListAsync();
+
+            return phones;
+        }
+    }
+}

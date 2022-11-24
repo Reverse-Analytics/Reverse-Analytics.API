c16f106bef59cb0bbc7a6e490ecc02a65b3341f1
Mirazyzz <khidoyatov1997@gmail.com>
2022-11-24 16:13:32 +0100

    Add sale & sale detail DTOs

---
+++ Reverse-Analytics/ReverseAnalytics.Domain/DTOs/Sale/SaleForCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using ReverseAnalytics.Domain.Enums;
+
+namespace ReverseAnalytics.Domain.DTOs.Sale
+{
+    public class SaleForCreateDto
+    {
+        public string Receipt { get; set; }
+        public string? Comment { get; set; }
+        public decimal TotalDue { get; set; }
+        public decimal TotalPaid { get; set; }
+        public decimal? DiscountPercentage { get; set; }
+        public decimal? DiscountTotal { get; set; }
+        public DateTime SaleDate { get; set; }
+        public SaleType SaleType { get; set; }
+
+        public int CustomerId { get; set; }
+    }
+}
---
+++ Reverse-Analytics/ReverseAnalytics.Domain/DTOs/Sale/SaleForUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿using ReverseAnalytics.Domain.Enums;
+
+namespace ReverseAnalytics.Domain.DTOs.Sale
+{
+    public class SaleForUpdateDto
+    {
+        public int Id { get; set; }
+        public string Receipt { get; set; }
+        public string? Comment { get; set; }
+        public decimal TotalDue { get; set; }
+        public decimal TotalPaid { get; set; }
+        public decimal? DiscountPercentage { get; set; }
+        public decimal? DiscountTotal { get; set; }
+        public DateTime SaleDate { get; set; }
+        public SaleType SaleType { get; set; }
+
+        public int CustomerId { get; set; }
+    }
+}

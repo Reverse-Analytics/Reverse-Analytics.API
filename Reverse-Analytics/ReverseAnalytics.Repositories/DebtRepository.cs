611501982aa43eaa576d64673a59b8dd33d0fe78
Mirazyzz <khidoyatov1997@gmail.com>
2022-11-24 13:07:15 +0100

    Add Debt repository implementation

---
+++ Reverse-Analytics/ReverseAnalytics.Repositories/DebtRepository.cs
@@ -0,0 +1,14 @@
+﻿using ReverseAnalytics.Domain.Entities;
+using ReverseAnalytics.Domain.Interfaces.Repositories;
+using ReverseAnalytics.Infrastructure.Persistence;
+
+namespace ReverseAnalytics.Repositories
+{
+    internal class DebtRepository : RepositoryBase<Debt>, IDebtRepository
+    {
+        public DebtRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+    }
+}

4e4edfb968cb24dde6c9c3d9ba55bfd38f674c19
Mirazyzz <khidoyatov1997@gmail.com>
2022-11-24 12:42:40 +0100

    Implement Address repository

---
+++ Reverse-Analytics/ReverseAnalytics.Domain/Interfaces/Repositories/IAddressRepository.cs
@@ -0,0 +1,9 @@
+﻿using ReverseAnalytics.Domain.Entities;
+
+namespace ReverseAnalytics.Domain.Interfaces.Repositories
+{
+    public interface IAddressRepository : IRepositoryBase<Address>
+    {
+        public Task<IEnumerable<Address>> FindAllByPersonId(int id);
+    }
+}
---
+++ Reverse-Analytics/ReverseAnalytics.Repositories/AddressRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReverseAnalytics.Domain.Entities;
+using ReverseAnalytics.Domain.Interfaces.Repositories;
+using ReverseAnalytics.Infrastructure.Persistence;
+
+namespace ReverseAnalytics.Repositories
+{
+    public class AddressRepository : RepositoryBase<Address>, IAddressRepository
+    {
+        public AddressRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Address>> FindAllByPersonId(int id)
+        {
+            var addresses = await _context.Addresses
+                .Where(a => a.PersonId == id)
+                .ToListAsync();
+
+            return addresses;
+        }
+    }
+}

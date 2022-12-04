c3381d38b24face9b2c8da5677ad40241e12ffdc
Mirazyzz <khidoyatov1997@gmail.com>
2022-12-04 15:42:15 +0100

    Refactor products controller
    Remove logging, use validation filter instead of model.state, remove try-catch blocks since errors are handled globally, improve variable and method naming.

--- Reverse-Analytics/Reverse-Analytics.Api/Controllers/ProductsController.cs
+++ Reverse-Analytics/Reverse-Analytics.Api/Controllers/ProductsController.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Reverse_Analytics.Api.Filters;
 using ReverseAnalytics.Domain.DTOs.Product;
 using ReverseAnalytics.Domain.Entities;
-using ReverseAnalytics.Domain.Exceptions;
 using ReverseAnalytics.Domain.Interfaces.Services;
 
 namespace Reverse_Analytics.Api.Controllers
@@ -22,7 +22,7 @@ namespace Reverse_Analytics.Api.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Product>>> GetProductsAsync(string? searchString, int? categoryId, int pageSize = pageSize, int pageNumber = 1)
+        public async Task<ActionResult<IEnumerable<Product>>> GetAllProductsAsync(string? searchString, int? categoryId, int pageSize = pageSize, int pageNumber = 1)
         {
             var products = await _service.GetProductsAsync(searchString, categoryId, pageSize, pageNumber);
 
@@ -40,100 +40,36 @@ namespace Reverse_Analytics.Api.Controllers
             return Ok(product);
         }
 
-        // POST api/<ProductController>
         [HttpPost]
-        public async Task<ActionResult<ProductDto>> Post(ProductForCreateDto productToCreate)
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<ActionResult<ProductDto>> CreateProductAsync([FromBody] ProductForCreateDto productToCreate)
         {
-            try
-            {
-                if(productToCreate is null)
-                {
-                    return BadRequest("Product cannot be null.");
-                }
+            var product = await _service.CreateProductAsync(productToCreate);
 
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest("Product is not valid for creation.");
-                }
+            if (product is null)
+                return StatusCode(500, "Unknown error has occured while creating new product.");
 
-                var product = await _service.CreateProductAsync(productToCreate);
-
-                if(product is null)
-                {
-                    return StatusCode(500, "Unknown error has occured while creating new product.");
-                }
-
-                return Ok(product);
-            }
-            catch(Exception ex)
-            {
-                _logger.LogError($"Error while creating a new product.", ex.Message);
-
-                return StatusCode(500, "There was an error creating a new product.");
-            }
+            return Created("Product was sucessfully created.", product);
         }
 
-        // PUT api/<ProductController>/5
         [HttpPut("{id}")]
-        public async Task<ActionResult> Put(int id, ProductForUpdateDto productToUpdate)
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<ActionResult> UpdateProductAsync([FromBody] ProductForUpdateDto productToUpdate, int id)
         {
-            try
-            {
-                if (productToUpdate is null)
-                {
-                    return BadRequest("Product cannot be null.");
-                }
+            if (productToUpdate.Id != id)
+                return BadRequest($"Product id: {productToUpdate.Id} does not match with route id: {id}.");
 
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest("Product is not valid for updating.");
-                }
+            await _service.UpdateProductAsync(productToUpdate);
 
-                if (productToUpdate.Id != id)
-                {
-                    return BadRequest($"Product id: {productToUpdate.Id} does not match with route id: {id}.");
-                }
-
-                await _service.UpdateProductAsync(productToUpdate);
-                
-                return NoContent();
-            }
-            catch(NotFoundException ex)
-            {
-                _logger.LogError($"Unable to find product with id: {id} to update.", ex.Message);
-
-                return NotFound($"Product with id: {id} does not exist.");
-            }
-            catch(Exception ex)
-            {
-                _logger.LogError($"Error updating product with id: {id}.", ex.Message);
-
-                return StatusCode(500, "Unknown error has occured while updating the product.");
-            }
+            return NoContent();
         }
 
-        // DELETE api/<ProductController>/5
         [HttpDelete("{id}")]
-        public async Task<ActionResult> Delete(int id)
+        public async Task<ActionResult> DeleteProductAsync(int id)
         {
-            try
-            {
-                await _service.DeleteProductAsync(id);
-                
-                return NoContent();
-            }
-            catch(NotFoundException ex)
-            {
-                _logger.LogError($"Unable to find product with id: {id} to delete.", ex.Message);
+            await _service.DeleteProductAsync(id);
 
-                return NotFound($"Product with id: {id} does not exist.");
-            }
-            catch(Exception ex)
-            {
-                _logger.LogError($"Error deleting product with id: {id}.", ex.Message);
-
-                return StatusCode(500, "Unknown error has occured while deleting the product.");
-            }
+            return NoContent();
         }
     }
 }

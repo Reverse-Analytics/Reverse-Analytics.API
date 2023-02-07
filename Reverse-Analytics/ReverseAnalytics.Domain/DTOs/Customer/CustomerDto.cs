5ba0b5805597db5304cb2d1633cbcdd760e29aa0
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-02-07 14:42:21 +0500

    Update DTOs

--- Reverse-Analytics/ReverseAnalytics.Domain/DTOs/Customer/CustomerDto.cs
+++ Reverse-Analytics/ReverseAnalytics.Domain/DTOs/Customer/CustomerDto.cs
@@ -7,9 +7,10 @@ namespace ReverseAnalytics.Domain.DTOs.Customer
         public int Id { get; set; }
         public string FullName { get; set; }
         public string? CompanyName { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
         public decimal Balance { get; set; }
         public bool IsActive { get; set; }
-        public string PrimaryPhone { get; set; }
 
         public ICollection<DebtDto> Debts { get; set; }
 
--- Reverse-Analytics/ReverseAnalytics.Domain/DTOs/Supplier/SupplierDto.cs
+++ Reverse-Analytics/ReverseAnalytics.Domain/DTOs/Supplier/SupplierDto.cs
@@ -7,6 +7,8 @@ namespace ReverseAnalytics.Domain.DTOs.Supplier
         public int Id { get; set; }
         public string FullName { get; set; }
         public string? CompanyName { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
         public decimal Balance { get; set; }
         public bool IsActive { get; set; }
 

3bebce025e07072738283702d2ca3782c552abac
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-02-07 10:52:46 +0500

    Remove separate entities for address & phones
    Move address & phone details to a person entity instead of one to many relationships with separate entities

--- Reverse-Analytics/ReverseAnalytics.Domain/Entities/Person.cs
+++ Reverse-Analytics/ReverseAnalytics.Domain/Entities/Person.cs
@@ -6,11 +6,11 @@ namespace ReverseAnalytics.Domain.Entities
     {
         public string FullName { get; set; }
         public string? CompanyName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
         public decimal Balance { get; set; }
         public bool IsActive { get; set; }
 
-        public virtual ICollection<Address> Addresses { get; set; }
-        public virtual ICollection<Phone> Phones { get; set; }
         public virtual ICollection<Debt> Debts { get; set; }
 
         public Person()
@@ -21,8 +21,6 @@ namespace ReverseAnalytics.Domain.Entities
         {
             FullName = fullName;
 
-            Addresses = new List<Address>();
-            Phones = new List<Phone>();
             Debts = new List<Debt>();
         }
     }

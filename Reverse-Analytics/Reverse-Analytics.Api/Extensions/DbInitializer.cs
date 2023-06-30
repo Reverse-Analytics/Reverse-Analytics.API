f3da1bada06f9cd8296f44d4eaf54ccb6e364d33
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-07-01 01:31:12 +0500

    Update DbInitializer.cs

--- Reverse-Analytics/Reverse-Analytics.Api/Extensions/DbInitializer.cs
+++ Reverse-Analytics/Reverse-Analytics.Api/Extensions/DbInitializer.cs
@@ -11,7 +11,7 @@ namespace Reverse_Analytics.Api.Extensions
     {
         public static IApplicationBuilder SeedDatabase(this IApplicationBuilder app)
         {
-            ArgumentNullException.ThrowIfNull(app, nameof(app));
+            ArgumentNullException.ThrowIfNull(app);
 
             using var scope = app.ApplicationServices.CreateScope();
             var services = scope.ServiceProvider;
@@ -19,10 +19,8 @@ namespace Reverse_Analytics.Api.Extensions
             try
             {
                 var context = services.GetRequiredService<ApplicationDbContext>();
-                var identityContext = services.GetRequiredService<ApplicationIdentityDbContext>();
-                var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
 
-                DbSeeder.Initialize(context, identityContext, userManager);
+                DbSeeder.Initialize(context);
             }
             catch (Exception ex)
             {
@@ -33,25 +31,29 @@ namespace Reverse_Analytics.Api.Extensions
         }
     }
 
-    internal class DbSeeder
+    internal static class DbSeeder
     {
-        private static readonly Faker _faker = new();
+        private static readonly Faker _faker = new("ru");
         private static readonly Random _random = new();
+        private static readonly DateTime minDate = DateTime.Now.AddYears(-2);
+        private static readonly DateTime maxDate = DateTime.Now;
 
-        public static void Initialize(ApplicationDbContext context, ApplicationIdentityDbContext identityContext, UserManager<IdentityUser> userManager)
+        public static void Initialize(ApplicationDbContext context)
         {
             try
             {
-                //CreateProductCategories(context);
-                //CreateProducts(context);
-                //CreateCustomers(context);
-                //CreateSales(context);
-                //CreateSaleDetails(context);
-                //CreateSaleDebts(context);
-                //CreateSuppliers(context);
-                //CreateSupplyDebts(context);
-                //CreateSupplies(context);
-                //CreateSupplyDetails(context);
+                CreateProductCategories(context);
+                CreateProducts(context);
+                CreateCustomers(context);
+                CreateSales(context);
+                CreateSaleDetails(context);
+                CreateSaleDebts(context);
+                CreateRefunds(context);
+                CreateRefundDetails(context);
+                CreateSuppliers(context);
+                CreateSupplies(context);
+                CreateSupplyDetails(context);
+                CreateSupplyDebts(context);
                 //CreateInventories(context);
                 //CreateInventoryDetails(context);
                 //CreateRoles(identityContext);
@@ -69,9 +71,9 @@ namespace Reverse_Analytics.Api.Extensions
             if (context.ProductCategories.Any()) return;
 
             List<ProductCategory> productCategories = new();
-            var fakeCategories = _faker.Commerce.Categories(25);
+            var fakeCategories = new Faker("ru").Commerce.Categories(20);
 
-            for (int i = 0; i < 25; i++)
+            for (int i = 0; i < fakeCategories.Length; i++)
             {
                 productCategories.Add(
                     new ProductCategory()
@@ -88,31 +90,60 @@ namespace Reverse_Analytics.Api.Extensions
         {
             if (context.Products.Any()) return;
 
-            var categories = context.ProductCategories.ToList();
-            List<Product> products = new();
+            var categories = context.ProductCategories.Select(x => x.Id).ToList();
+            var productFaker = new Faker<Product>("ru")
+                .RuleFor(x => x.ProductName, f => f.Commerce.ProductName())
+                .RuleFor(x => x.ProductCode, f => new Faker().Lorem.Letter(2))
+                .RuleFor(x => x.Description, f => f.Commerce.ProductDescription())
+                .RuleFor(x => x.SupplyPrice, f => f.Random.Decimal(10000, 50000))
+                .RuleFor(x => x.SalePrice, f => f.Random.Decimal(15000, 80000))
+                .RuleFor(x => x.Volume, f => f.Random.Double(10, 100))
+                .RuleFor(x => x.Weight, f => f.Random.Double(15, 150))
+                .RuleFor(x => x.UnitOfMeasurement, f => f.Random.Enum<UnitOfMeasurement>())
+                .RuleFor(x => x.QuantityInStock, f => f.Random.Int(0, 100))
+                .RuleFor(x => x.CategoryId, f => f.Random.ListItem(categories))
+                .Generate(100);
 
-            foreach (var category in categories)
-            {
-                int numberOfProducts = _random.Next(1, 25);
+            context.Products.AddRange(productFaker);
+            context.SaveChanges();
+        }
 
-                for (int i = 0; i < numberOfProducts; i++)
-                {
-                    products.Add(
-                        new Product()
-                        {
-                            ProductCode = _faker.Commerce.ProductAdjective(),
-                            ProductName = _faker.Commerce.ProductName(),
-                            Volume = (double)(_random.NextDouble() * _random.Next(1, 500)),
-                            Weight = (double)(_random.NextDouble() * _random.Next(1, 600)),
-                            SupplyPrice = Math.Round((decimal)_random.NextDouble() * 500, 2),
-                            SalePrice = Math.Round((decimal)_random.NextDouble() * 800, 2),
-                            UnitOfMeasurement = _faker.Random.Enum<UnitOfMeasurement>(),
-                            CategoryId = category.Id
-                        });
-                }
-            }
+        private static void CreateCustomers(ApplicationDbContext context)
+        {
+            if (context.Customers.Any()) return;
 
-            context.Products.AddRange(products);
+            var customerFaker = new Faker<Customer>("ru")
+                .RuleFor(x => x.FullName, f => f.Person.FullName)
+                .RuleFor(x => x.PhoneNumber, f => f.Phone.PhoneNumber("+998-9#-###-##-##"))
+                .RuleFor(x => x.Company, f => f.Company.CompanyName())
+                .RuleFor(x => x.Address, f => f.Address.FullAddress())
+                .RuleFor(x => x.Balance, f => f.Random.Decimal(0, 500000))
+                .RuleFor(x => x.Discount, f => f.Random.Double(0, 50))
+                .Generate(30);
+
+            context.Customers.AddRange(customerFaker);
+            context.SaveChanges();
+        }
+
+        private static void CreateSales(ApplicationDbContext context)
+        {
+            if (context.Sales.Any()) return;
+
+
+            var customers = context.Customers.Select(x => x.Id).ToList();
+            var fakeSales = new Faker<Sale>("ru")
+                .RuleFor(x => x.Receipt, f => f.Random.Guid().ToString()[..8])
+                .RuleFor(x => x.Comments, f => f.Commerce.ProductAdjective())
+                .RuleFor(x => x.SoldBy, f => f.Person.FirstName)
+                .RuleFor(x => x.TotalDue, f => f.Random.Decimal(10000, 5000000))
+                .RuleFor(x => x.TotalPaid, f => f.Random.Decimal(5000, 5500000))
+                .RuleFor(x => x.TotalDiscount, f => f.Random.Decimal(1000, 500000))
+                .RuleFor(x => x.SaleType, f => f.Random.Enum<SaleType>())
+                .RuleFor(x => x.SaleDate, f => f.Date.Between(minDate, maxDate))
+                .RuleFor(x => x.CustomerId, f => f.Random.ListItem(customers))
+                .Generate(100);
+
+            context.Sales.AddRange(fakeSales);
             context.SaveChanges();
         }
 
@@ -120,28 +151,99 @@ namespace Reverse_Analytics.Api.Extensions
         {
             if (context.SaleDetails.Any()) return;
 
-            var sales = context.Sales.ToList();
-            var products = context.Products.ToList();
-            List<SaleDetail> orderItems = new();
+            var sales = context.Sales.Select(x => x.Id).ToList();
+            var products = context.Products.Select(x => x.Id).ToList();
+            var saleDetailsFaker = new Faker<SaleDetail>("ru")
+                .RuleFor(x => x.Quantity, f => f.Random.Int(1, 50))
+                .RuleFor(x => x.UnitPrice, f => f.Random.Decimal(5000, 50000))
+                .RuleFor(x => x.Discount, f => f.Random.Decimal(500, 20000))
+                .RuleFor(x => x.SaleId, f => f.Random.ListItem(sales))
+                .RuleFor(x => x.ProductId, f => f.Random.ListItem(products))
+                .Generate(500);
 
-            foreach (var sale in sales)
-            {
-                var orderItemsCount = _random.Next(1, 15);
+            context.SaleDetails.AddRange(saleDetailsFaker);
+            context.SaveChanges();
+        }
 
-                for (int i = 0; i < orderItemsCount; i++)
-                {
-                    orderItems.Add(
-                        new SaleDetail()
-                        {
-                            Quantity = _random.Next(1, 20),
-                            UnitPrice = decimal.Round(_faker.Random.Decimal(5, 500), 2),
-                            SaleId = sale.Id,
-                            ProductId = products[_random.Next(0, products.Count)]?.Id ?? 1
-                        });
-                }
-            }
+        private static void CreateSaleDebts(ApplicationDbContext context)
+        {
+            if (context.SaleDebts.Any()) return;
 
-            context.SaleDetails.AddRange(orderItems);
+            var sales = context.Sales.Select(x => x.Id).ToList();
+            var debtsFaker = new Faker<SaleDebt>("ru")
+                .RuleFor(x => x.TotalDue, f => f.Random.Decimal(10000, 500000))
+                .RuleFor(x => x.DueDate, f => f.Date.Between(minDate, maxDate))
+                .RuleFor(x => x.ClosedDate, f => f.Date.Between(minDate, maxDate))
+                .RuleFor(x => x.Status, f => f.Random.Enum<DebtStatus>())
+                .RuleFor(x => x.SaleId, f => f.Random.ListItem(sales))
+                .Generate(25);
+
+            context.SaleDebts.AddRange(debtsFaker);
+            context.SaveChanges();
+        }
+
+        private static void CreateRefunds(ApplicationDbContext context)
+        {
+            if (context.Refunds.Any()) return;
+
+            var sales = context.Sales.Select(x => x.Id).ToList();
+            var refundFaker = new Faker<Refund>("ru")
+                .RuleFor(x => x.RefundDate, f => f.Date.Between(minDate, maxDate))
+                .RuleFor(x => x.Reason, f => f.Lorem.Text())
+                .RuleFor(x => x.ReceivedBy, f => f.Person.FirstName)
+                .RuleFor(x => x.SaleId, f => f.Random.ListItem(sales))
+                .Generate(25);
+
+            context.Refunds.AddRange(refundFaker);
+            context.SaveChanges();
+        }
+
+        private static void CreateRefundDetails(ApplicationDbContext context)
+        {
+            if (context.RefundDetails.Any()) return;
+
+            var refunds = context.Refunds.Select(x => x.Id).ToList();
+            var products = context.Products.Select(x => x.Id).ToList();
+            var detailsFaker = new Faker<RefundDetail>("ru")
+                .RuleFor(x => x.Quantity, f => f.Random.Int(1, 10))
+                .RuleFor(x => x.RefundId, f => f.Random.ListItem(refunds))
+                .RuleFor(x => x.ProductId, f => f.Random.ListItem(products))
+                .Generate(150);
+
+            context.RefundDetails.AddRange(detailsFaker);
+            context.SaveChanges();
+        }
+
+        private static void CreateSuppliers(ApplicationDbContext context)
+        {
+            if (context.Suppliers.Any()) return;
+
+            var suppliersFaker = new Faker<Supplier>("ru")
+                .RuleFor(x => x.FullName, f => f.Person.FullName)
+                .RuleFor(x => x.PhoneNumber, f => f.Phone.PhoneNumber("+998-9#-###-##-##"))
+                .RuleFor(x => x.Company, f => f.Company.CompanyName())
+                .RuleFor(x => x.Balance, f => f.Random.Decimal(0, 500000))
+                .Generate(30);
+
+            context.Suppliers.AddRange(suppliersFaker);
+            context.SaveChanges();
+        }
+
+        private static void CreateSupplies(ApplicationDbContext context)
+        {
+            if (context.Supplies.Any()) return;
+
+            var suppliers = context.Suppliers.Select(x => x.Id).ToList();
+            var suppliesFaker = new Faker<Supply>("ru")
+                .RuleFor(x => x.ReceivedBy, f => f.Person.FirstName)
+                .RuleFor(x => x.Comment, f => f.Lorem.Sentence())
+                .RuleFor(x => x.TotalDue, f => f.Random.Decimal(100000, 5000000))
+                .RuleFor(x => x.TotalPaid, f => f.Random.Decimal(50000, 5500000))
+                .RuleFor(x => x.SupplyDate, f => f.Date.Between(minDate, maxDate))
+                .RuleFor(x => x.SupplierId, f => f.Random.ListItem(suppliers))
+                .Generate(100);
+
+            context.Supplies.AddRange(suppliesFaker);
             context.SaveChanges();
         }
 
@@ -149,31 +251,35 @@ namespace Reverse_Analytics.Api.Extensions
         {
             if (context.SupplyDetails.Any()) return;
 
-            var supplies = context.Supplies.ToList();
-            var products = context.Products.ToList();
-            List<SupplyDetail> supplyDetails = new List<SupplyDetail>();
+            var supplies = context.Supplies.Select(x => x.Id).ToList();
+            var products = context.Products.Select(x => x.Id).ToList();
+            var detailsFaker = new Faker<SupplyDetail>("ru")
+                .RuleFor(x => x.Quantity, f => f.Random.Int(1, 50))
+                .RuleFor(x => x.UnitPrice, f => f.Random.Decimal(5000, 50000))
+                .RuleFor(x => x.SupplyId, f => f.Random.ListItem(supplies))
+                .RuleFor(x => x.ProductId, f => f.Random.ListItem(products))
+                .Generate(350);
 
-            foreach (var supply in supplies)
-            {
-                int suppliesCount = _random.Next(1, 15);
-
-                for (int i = 0; i < suppliesCount; i++)
-                {
-                    supplyDetails.Add(new SupplyDetail()
-                    {
-                        Quantity = _random.Next(1, 20),
-                        UnitPrice = decimal.Round(_faker.Random.Decimal(5, 500), 2),
-
-                        SupplyId = supply.Id,
-                        ProductId = products[_random.Next(0, products.Count)]?.Id ?? 1
-                    });
-                }
-            }
-
-            context.SupplyDetails.AddRange(supplyDetails);
+            context.SupplyDetails.AddRange(detailsFaker);
             context.SaveChanges();
         }
 
+        private static void CreateSupplyDebts(ApplicationDbContext context)
+        {
+            if (context.SupplyDebts.Any()) return;
+
+            var supplies = context.Supplies.Select(x => x.Id).ToList();
+            var debtsFaker = new Faker<SupplyDebt>("ru")
+                .RuleFor(x => x.TotalDue, f => f.Random.Decimal(10000, 500000))
+                .RuleFor(x => x.DueDate, f => f.Date.Between(minDate, maxDate))
+                .RuleFor(x => x.ClosedDate, f => f.Date.Between(minDate, maxDate))
+                .RuleFor(x => x.Status, f => f.Random.Enum<DebtStatus>())
+                .RuleFor(x => x.SupplyId, f => f.Random.ListItem(supplies))
+                .Generate(25);
+
+            context.SupplyDebts.AddRange(debtsFaker);
+            context.SaveChanges();
+        }
 
         private static void CreateRoles(ApplicationIdentityDbContext context)
         {

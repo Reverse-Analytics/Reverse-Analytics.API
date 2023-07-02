a1bf9d77b339acbd2b955d54870997594796c48c
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-07-02 22:09:15 +0500

    Update SupplyConfiguration.cs

--- Reverse-Analytics/ReverseAnalytics.Infrastructure/Persistence/Configurations/SupplyConfiguration.cs
+++ Reverse-Analytics/ReverseAnalytics.Infrastructure/Persistence/Configurations/SupplyConfiguration.cs
@@ -17,13 +17,7 @@ namespace ReverseAnalytics.Infrastructure.Persistence.Configurations
             builder.Property(s => s.ReceivedBy)
                 .HasMaxLength(500)
                 .IsRequired(false);
-            builder.Property(s => s.Comment)
-                .HasMaxLength(2500)
-                .IsRequired(false);
             builder.Property(x => x.TotalDue)
-                .HasColumnType("money")
-                .HasPrecision(18, 2);
-            builder.Property(x => x.TotalPaid)
                 .HasColumnType("money")
                 .HasPrecision(18, 2);
         }

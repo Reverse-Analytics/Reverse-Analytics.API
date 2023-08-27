1a8d12f2babf1ef5f819afa2feeeba2617944942
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-08-27 15:01:16 +0500

    Add methods to sale repository

--- Reverse-Analytics/ReverseAnalytics.Domain/Interfaces/Repositories/ISaleRepository.cs
+++ Reverse-Analytics/ReverseAnalytics.Domain/Interfaces/Repositories/ISaleRepository.cs
@@ -4,5 +4,7 @@ namespace ReverseAnalytics.Domain.Interfaces.Repositories
 {
     public interface ISaleRepository : IRepositoryBase<Sale>
     {
+        public Task<IEnumerable<Sale>> FindAllAsync();
+        public Task<IEnumerable<Sale>> FindAllByCustomerIdAsync(int customerId);
     }
 }
--- Reverse-Analytics/ReverseAnalytics.Repositories/SaleRepository.cs
+++ Reverse-Analytics/ReverseAnalytics.Repositories/SaleRepository.cs
@@ -1,4 +1,5 @@
-﻿using ReverseAnalytics.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using ReverseAnalytics.Domain.Entities;
 using ReverseAnalytics.Domain.Interfaces.Repositories;
 using ReverseAnalytics.Infrastructure.Persistence;
 
@@ -9,6 +10,24 @@ namespace ReverseAnalytics.Repositories
         public SaleRepository(ApplicationDbContext context)
             : base(context)
         {
+        }
+
+        public async Task<IEnumerable<Sale>> FindAllAsync()
+        {
+            return await _context.Sales
+                .Include(x => x.Customer)
+                .Include(x => x.OrderDetails)
+                .Include(x => x.Refunds)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Sale>> FindAllByCustomerIdAsync(int customerId)
+        {
+            return await _context.Sales
+                .Where(x => x.CustomerId == customerId)
+                .AsNoTracking()
+                .ToListAsync();
         }
     }
 }

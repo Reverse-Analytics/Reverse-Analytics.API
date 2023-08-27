15435f8d0cd65a32e95b2fe9265712432559d623
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-08-27 15:00:41 +0500

    Update debt dtos

---
+++ Reverse-Analytics/ReverseAnalytics.Domain/DTOs/SaleDebt/DebtPaymentDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ReverseAnalytics.Domain.DTOs.SaleDebt
+{
+    public class DebtPaymentDto
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+    }
+}

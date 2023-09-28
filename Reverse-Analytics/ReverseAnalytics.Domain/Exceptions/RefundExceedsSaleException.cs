10c525efe4f3af15210ae9d148b24c8581538f1f
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-09-28 15:38:14 +0500

    Create RefundExceedsSaleException.cs

---
+++ Reverse-Analytics/ReverseAnalytics.Domain/Exceptions/RefundExceedsSaleException.cs
@@ -0,0 +1,20 @@
+﻿namespace ReverseAnalytics.Domain.Exceptions
+{
+    public class RefundExceedsSaleException : Exception
+    {
+        public RefundExceedsSaleException()
+            : base()
+        {
+        }
+
+        public RefundExceedsSaleException(string message)
+            : base(message)
+        {
+        }
+
+        public RefundExceedsSaleException(string message, Exception ex)
+            : base(message, ex)
+        {
+        }
+    }
+}

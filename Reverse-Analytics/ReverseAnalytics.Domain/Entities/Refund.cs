6f659d88b9f15c80c43ebd10c8d0b74c3ecb60e8
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2023-10-01 21:41:35 +0500

    Add column which indicates whether refund covered debt

--- Reverse-Analytics/ReverseAnalytics.Domain/Entities/Refund.cs
+++ Reverse-Analytics/ReverseAnalytics.Domain/Entities/Refund.cs
@@ -5,6 +5,8 @@ namespace ReverseAnalytics.Domain.Entities
     public class Refund : BaseAuditableEntity
     {
         public decimal TotalAmount { get; set; }
+        public decimal DebtPaymentAmount { get; set; }
+        public decimal RefundAmount { get; set; }
         public DateTime RefundDate { get; set; }
         public string? Reason { get; set; }
         public string? ReceivedBy { get; set; }

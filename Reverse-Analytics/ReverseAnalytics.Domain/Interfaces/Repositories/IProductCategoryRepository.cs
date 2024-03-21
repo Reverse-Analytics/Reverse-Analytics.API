1b3735da152393e6b595743171295449baf4cd77
Miraziz.Khidoyatov <khidoyatov1997@gmail.com>
2024-03-22 03:13:35 +0500

    update category repository

--- Reverse-Analytics/ReverseAnalytics.Domain/Interfaces/Repositories/IProductCategoryRepository.cs
+++ Reverse-Analytics/ReverseAnalytics.Domain/Interfaces/Repositories/IProductCategoryRepository.cs
@@ -1,7 +1,11 @@
-﻿using ReverseAnalytics.Domain.Entities;
+﻿using ReverseAnalytics.Domain.Common;
+using ReverseAnalytics.Domain.Entities;
+using ReverseAnalytics.Domain.QueryParameters;
 
 namespace ReverseAnalytics.Domain.Interfaces.Repositories;
 
 public interface IProductCategoryRepository : IRepositoryBase<ProductCategory>
 {
+    Task<PaginatedList<ProductCategory>> FindAllAsync(ProductCategoryQueryParameters queryParameters);
+    Task<IEnumerable<ProductCategory>> FindByParentIdAsync(int parentId);
 }
--- Reverse-Analytics/ReverseAnalytics.Infrastructure/Repositories/ProductCategoryRepository.cs
+++ Reverse-Analytics/ReverseAnalytics.Infrastructure/Repositories/ProductCategoryRepository.cs
@@ -2,7 +2,7 @@
 using ReverseAnalytics.Domain.Common;
 using ReverseAnalytics.Domain.Entities;
 using ReverseAnalytics.Domain.Interfaces.Repositories;
-using ReverseAnalytics.Domain.ResourceParameters;
+using ReverseAnalytics.Domain.QueryParameters;
 using ReverseAnalytics.Infrastructure.Extensions;
 using ReverseAnalytics.Infrastructure.Persistence;
 
@@ -10,7 +10,7 @@ namespace ReverseAnalytics.Infrastructure.Repositories;
 
 public class ProductCategoryRepository(ApplicationDbContext context) : RepositoryBase<ProductCategory>(context), IProductCategoryRepository
 {
-    public override async Task<PaginatedList<ProductCategory>> FindAllAsync(PaginatedQueryParameters queryParameters)
+    public async Task<PaginatedList<ProductCategory>> FindAllAsync(ProductCategoryQueryParameters queryParameters)
     {
         var query = _context.ProductCategories.AsQueryable();
 
@@ -19,11 +19,28 @@ public class ProductCategoryRepository(ApplicationDbContext context) : Repositor
             query = query.Where(x => x.Name.Contains(queryParameters.SearchQuery, StringComparison.OrdinalIgnoreCase));
         }
 
+        if (queryParameters.ParentId is not null)
+        {
+            query = query.Where(x => x.ParentId == queryParameters.ParentId);
+        }
+
         var entities = await query
             .Include(x => x.Products)
-            .AsNoTracking()
+            .Include(x => x.Parent)
+            .Include(x => x.SubCategories)
+            .AsNoTrackingWithIdentityResolution()
             .ToPaginatedListAsync(queryParameters.PageNumber, queryParameters.PageSize);
 
         return entities;
+    }
+
+    public async Task<IEnumerable<ProductCategory>> FindByParentIdAsync(int parentId)
+    {
+        var categories = await _context.ProductCategories
+            .Where(x => x.ParentId == parentId)
+            .AsNoTracking()
+            .ToListAsync();
+
+        return categories;
     }
 }
